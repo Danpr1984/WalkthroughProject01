ac58f63c2b3bc5d77a7594cb63cf567144cd5061
Gyan Shashwat <gyan.shashwat@ucdconnect.ie>
2021-10-22 12:45:06 +0000

    pushed for dashboard

--- app_pages/page_cells_visualizer.py
+++ app_pages/page_cells_visualizer.py
@@ -30,7 +30,7 @@ def page_cells_visualizer_body():
       st.image(avg_uninfected, caption='Uninfected Cell - Average and Variability')
       st.write("---")
 
-    if st.checkbox("Differences between average parasitized and average uninfected cells."):
+    if st.checkbox("Differences between average parasitized and average uninfected cells"):
           diff_between_avgs = plt.imread(f"outputs/{version}/avg_diff.png")
 
           st.warning(
